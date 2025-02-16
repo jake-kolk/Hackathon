eec2e6664a7a902c92cace1856ca7a12a540bd66
Gavin A Burchett <102335590+McGavGav@users.noreply.github.com>
2025-02-16 03:39:48 -0800

    stuff

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -10,14 +10,13 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
-#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QVBoxLayout>
@@ -30,20 +29,19 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QGridLayout *gridLayout;
+    QTextBrowser *responseBox;
+    QPushButton *pushButton;
+    QPushButton *sendButton;
+    QPushButton *pushButton_2;
+    QVBoxLayout *verticalLayout;
+    QGraphicsView *graphicsView;
     QWidget *gridWidget;
     QGridLayout *gridLayout_2;
     QTextBrowser *textBrowser;
     QTextBrowser *promptTextBox;
-    QVBoxLayout *verticalLayout;
-    QTextBrowser *responseBox;
-    QPushButton *pushButton;
-    QPushButton *sendButton;
-    QGraphicsView *graphicsView;
-    QPushButton *pushButton_2;
+    QSpacerItem *verticalSpacer;
     QStatusBar *statusbar;
     QMenuBar *menuBar;
-    QMenu *menuSave;
-    QMenu *menuClear;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -61,6 +59,36 @@ public:
         centralwidget->setMaximumSize(QSize(16777215, 16777215));
         gridLayout = new QGridLayout(centralwidget);
         gridLayout->setObjectName("gridLayout");
+        responseBox = new QTextBrowser(centralwidget);
+        responseBox->setObjectName("responseBox");
+
+        gridLayout->addWidget(responseBox, 1, 3, 1, 1);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+
+        gridLayout->addWidget(pushButton, 2, 4, 1, 1);
+
+        sendButton = new QPushButton(centralwidget);
+        sendButton->setObjectName("sendButton");
+
+        gridLayout->addWidget(sendButton, 2, 2, 1, 1);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+
+        gridLayout->addWidget(pushButton_2, 2, 3, 1, 1);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+
+        gridLayout->addLayout(verticalLayout, 1, 1, 1, 1);
+
+        graphicsView = new QGraphicsView(centralwidget);
+        graphicsView->setObjectName("graphicsView");
+
+        gridLayout->addWidget(graphicsView, 1, 4, 1, 1);
+
         gridWidget = new QWidget(centralwidget);
         gridWidget->setObjectName("gridWidget");
         gridWidget->setMaximumSize(QSize(10000, 10000));
@@ -71,7 +99,7 @@ public:
         textBrowser->setMinimumSize(QSize(0, 192));
         textBrowser->setAutoFillBackground(false);
 
-        gridLayout_2->addWidget(textBrowser, 1, 0, 1, 1, Qt::AlignmentFlag::AlignHCenter);
+        gridLayout_2->addWidget(textBrowser, 0, 0, 1, 1, Qt::AlignmentFlag::AlignHCenter);
 
         promptTextBox = new QTextBrowser(gridWidget);
         promptTextBox->setObjectName("promptTextBox");
@@ -79,38 +107,12 @@ public:
 
         gridLayout_2->addWidget(promptTextBox, 3, 0, 2, 1, Qt::AlignmentFlag::AlignHCenter);
 
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
 
-        gridLayout->addWidget(gridWidget, 2, 2, 1, 1);
+        gridLayout_2->addItem(verticalSpacer, 0, 1, 2, 1);
 
-        verticalLayout = new QVBoxLayout();
-        verticalLayout->setObjectName("verticalLayout");
 
-        gridLayout->addLayout(verticalLayout, 2, 1, 1, 1);
-
-        responseBox = new QTextBrowser(centralwidget);
-        responseBox->setObjectName("responseBox");
-
-        gridLayout->addWidget(responseBox, 2, 3, 1, 1);
-
-        pushButton = new QPushButton(centralwidget);
-        pushButton->setObjectName("pushButton");
-
-        gridLayout->addWidget(pushButton, 3, 4, 1, 1);
-
-        sendButton = new QPushButton(centralwidget);
-        sendButton->setObjectName("sendButton");
-
-        gridLayout->addWidget(sendButton, 3, 2, 1, 1);
-
-        graphicsView = new QGraphicsView(centralwidget);
-        graphicsView->setObjectName("graphicsView");
-
-        gridLayout->addWidget(graphicsView, 2, 4, 1, 1);
-
-        pushButton_2 = new QPushButton(centralwidget);
-        pushButton_2->setObjectName("pushButton_2");
-
-        gridLayout->addWidget(pushButton_2, 3, 3, 1, 1);
+        gridLayout->addWidget(gridWidget, 1, 2, 1, 1);
 
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
@@ -119,14 +121,7 @@ public:
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName("menuBar");
         menuBar->setGeometry(QRect(0, 0, 1178, 25));
-        menuSave = new QMenu(menuBar);
-        menuSave->setObjectName("menuSave");
-        menuClear = new QMenu(menuBar);
-        menuClear->setObjectName("menuClear");
         MainWindow->setMenuBar(menuBar);
-
-        menuBar->addAction(menuSave->menuAction());
-        menuBar->addAction(menuClear->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -136,6 +131,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Next ->", nullptr));
+        sendButton->setText(QCoreApplication::translate("MainWindow", "Make your story!", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "<- Back", nullptr));
         textBrowser->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
@@ -143,19 +141,22 @@ public:
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt; font-weight:700; text-decoration: underline;\">User Manual:</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt; font-weight:700; text-decoration: underline;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right"
-                        ":0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">Enter a topic for your story in the textbox below.</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">and click &quot;send&quot;</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">If you would like to save the story generated as</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">a .txt file click the &quot;save&quot; button</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; mar"
-                        "gin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">If you would like to clear the story hit &quot;Clear&quot;</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p></body></html>", nullptr));
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt; font-weight:700; text-decoration: underline;\">User Manual:</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt; font-weight:700; text-decoration: underline;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0p"
+                        "x; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">Enter a topic for your story in the textbox below.</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">and click &quot;Make your story!&quot;</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">If you would like to save the story generated as</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">a .tx"
+                        "t file click the &quot;Save&quot; button</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">If you would like to scrub your API key hit &quot;Clear API key&quot;</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">If you would like to clear the story hit &quot;Clear text&quot;</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margi"
+                        "n-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;\"><br /></p></body></html>", nullptr));
         promptTextBox->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
@@ -164,11 +165,6 @@ public:
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "Next ->", nullptr));
-        sendButton->setText(QCoreApplication::translate("MainWindow", "Make your story!", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "<- Back", nullptr));
-        menuSave->setTitle(QCoreApplication::translate("MainWindow", "Save", nullptr));
-        menuClear->setTitle(QCoreApplication::translate("MainWindow", "Clear", nullptr));
     } // retranslateUi
 
 };
