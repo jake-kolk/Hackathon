67fa0e561d54ed3689547a677d184113fe2fae3f
Gavin A Burchett <102335590+McGavGav@users.noreply.github.com>
2025-02-16 02:10:50 -0800

    added icon :)

--- Hackathon.pro
+++ Hackathon.pro
@@ -3,6 +3,8 @@ QT += network widgets
 QT += core network
 QT += multimedia
 
+
+
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 CONFIG += c++17
@@ -10,6 +12,9 @@ CONFIG += c++17
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+RC_FILE = otherResources.rc
+
 
 SOURCES += \
     apicaller.cpp \
@@ -35,3 +40,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
     resources.qrc
+
+DISTFILES += \
+    otherResources.rc
--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -50,7 +50,8 @@ static constexpr auto qt_meta_stringdata_ZN10MainWindowE = QtMocHelpers::stringD
     "QList<QImage>",
     "images",
     "nextImage",
-    "updateImage"
+    "updateImage",
+    "prevImage"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -62,7 +63,7 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -70,13 +71,14 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   56,    2, 0x08,    1 /* Private */,
-       3,    1,   57,    2, 0x08,    2 /* Private */,
-       5,    0,   60,    2, 0x08,    4 /* Private */,
-       6,    0,   61,    2, 0x08,    5 /* Private */,
-       7,    1,   62,    2, 0x08,    6 /* Private */,
-      10,    0,   65,    2, 0x08,    8 /* Private */,
-      11,    0,   66,    2, 0x08,    9 /* Private */,
+       1,    0,   62,    2, 0x08,    1 /* Private */,
+       3,    1,   63,    2, 0x08,    2 /* Private */,
+       5,    0,   66,    2, 0x08,    4 /* Private */,
+       6,    0,   67,    2, 0x08,    5 /* Private */,
+       7,    1,   68,    2, 0x08,    6 /* Private */,
+      10,    0,   71,    2, 0x08,    8 /* Private */,
+      11,    0,   72,    2, 0x08,    9 /* Private */,
+      12,    0,   73,    2, 0x08,   10 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -84,6 +86,7 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, 0x80000000 | 8,    9,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -114,6 +117,8 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         // method 'nextImage'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'updateImage'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'prevImage'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -131,6 +136,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 4: _t->imageHandler((*reinterpret_cast< std::add_pointer_t<QList<QImage>>>(_a[1]))); break;
         case 5: _t->nextImage(); break;
         case 6: _t->updateImage(); break;
+        case 7: _t->prevImage(); break;
         default: ;
         }
     }
@@ -167,14 +173,14 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     }
     if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     }
     return _id;
 }
